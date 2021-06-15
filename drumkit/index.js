74178aa70dd02f16525aee1b1fcb2fe4324ce801
Asif M <amohiuddeen@cloudera.com>
2021-06-15 22:01:37 +0530

    Adding Drum Kit Site to the Album

---
+++ drumkit/index.js
@@ -0,0 +1,40 @@
+numberOfbuttons = document.querySelectorAll('.drum').length;
+
+var audioMap = {
+  'w': 'tom-1',
+  'a': 'tom-2',
+  's': 'tom-3',
+  'd': 'tom-4',
+  'j': 'crash',
+  'k': 'kick-bass',
+  'l': 'snare'
+}
+
+for (var i=0; i<numberOfbuttons; i++) {
+  document.querySelectorAll('.drum')[i].addEventListener('click', function () {
+    playDrum(this.innerHTML);
+    addAnimation(this.innerHTML);
+  });
+}
+
+document.addEventListener('keydown', function (event) {
+  playDrum(event.key);
+  addAnimation(event.key);
+});
+
+function playDrum(key) {
+  var trackToPlay = audioMap[key];
+  if (trackToPlay) {
+    var curAudioFile = 'sounds/' + trackToPlay + '.mp3';
+    audio = new Audio(curAudioFile).play();
+  }
+}
+
+function addAnimation(currentKey) {
+  var animatingCSSClass = 'pressed';
+  var curButton = document.querySelector("." + currentKey);
+  curButton.classList.add(animatingCSSClass);
+  setTimeout(function () {
+    curButton.classList.remove(animatingCSSClass);
+  }, 100);
+}
