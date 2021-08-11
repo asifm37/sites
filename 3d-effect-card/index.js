c11c80e44387d196e44ce7707c7779268a551ca9
Asif M <amohiuddeen@cloudera.com>
2021-08-11 23:03:38 +0530

    Adding Mobile Support, image optimazation & button effects

--- 3d-effect-card/index.js
+++ 3d-effect-card/index.js
@@ -6,16 +6,25 @@ const desc = document.querySelector('.info h3');
 const sizes = document.querySelector('.sizes');
 const purchase = document.querySelector('.purchase');
 
-
 // Animation Event
-container.addEventListener('mousemove', (e) => {
-  let xAxis = (window.innerWidth / 2 - e.pageX) / 24;
-  let yAxis = (window.innerHeight / 2 - e.pageY) / 24;
-  card.style.transform = `rotateY(${xAxis}deg) rotateX(${yAxis}deg)`;
-})
+container.addEventListener('mousemove', (e) => rotation3d(e, 24)); //Desktop
+container.addEventListener('touchmove', (e) => rotation3d(e, 24)); //Mobile
 
 //Animate In
-container.addEventListener('mouseenter', e => {
+container.addEventListener('mouseenter', popOut3d);
+container.addEventListener('touchenter', popOut3d);
+
+//Animate Out - Restore
+container.addEventListener('mouseleave', popBack3d);
+container.addEventListener('touchleave', popBack3d);
+
+function rotation3d(e, scale = 24) {
+  let xAxis = (window.innerWidth / 2 - e.pageX) / scale;
+  let yAxis = (window.innerHeight / 2 - e.pageY) / scale;
+  card.style.transform = `rotateY(${xAxis}deg) rotateX(${yAxis}deg)`;
+}
+
+function popOut3d() {
   card.style.transition = 'none';
   // Element PopOut Effect
   sneaker.style.transform = `translateZ(200px) rotateZ(-45deg)`;
@@ -23,10 +32,9 @@ container.addEventListener('mouseenter', e => {
   desc.style.transform = `translateZ(125px)`;
   sizes.style.transform = `translateZ(100px)`;
   purchase.style.transform = `translateZ(75px)`;
-})
+}
 
-//Animate Out - Restore
-container.addEventListener('mouseleave', e => {
+function popBack3d() {
   card.style.transition = 'all 0.5s ease';
   card.style.transform = `rotateX(0deg) rotateY(0deg)`;
   // Reset / Popback Elements
@@ -35,4 +43,4 @@ container.addEventListener('mouseleave', e => {
   desc.style.transform = `translateZ(0px)`;
   sizes.style.transform = `translateZ(0px)`;
   purchase.style.transform = `translateZ(0px)`;
-})
+}
